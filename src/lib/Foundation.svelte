0d800e0eee716904f7166841eb4ba0894333f902
Dillon Fagan <dillonfagan@pm.me>
2023-08-05 17:51:36 -0400

    Remove unused Link component

--- src/lib/Foundation.svelte
+++ src/lib/Foundation.svelte
@@ -1,7 +1,6 @@
 <script context="module">
     export { default as Button } from "./foundation/Button.svelte";
     export { default as Column } from "./foundation/Column.svelte";
-    export { default as Link } from "./foundation/Link.svelte";
     export { default as Row } from "./foundation/Row.svelte";
     export { default as Text } from "./foundation/Text.svelte";
     export { default as TextInput } from "./foundation/TextInput.svelte";
