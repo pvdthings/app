7a07f307e15f17705f8ab3dc8f5102c2685a895f
Dillon Fagan <dillonfagan@pm.me>
2023-08-05 17:54:12 -0400

    Remove unused Text component

--- src/lib/Foundation.svelte
+++ src/lib/Foundation.svelte
@@ -2,7 +2,6 @@
     export { default as Button } from "./foundation/Button.svelte";
     export { default as Column } from "./foundation/Column.svelte";
     export { default as Row } from "./foundation/Row.svelte";
-    export { default as Text } from "./foundation/Text.svelte";
     export { default as TextInput } from "./foundation/TextInput.svelte";
     export { default as Title } from "./foundation/Title.svelte";
 </script>
