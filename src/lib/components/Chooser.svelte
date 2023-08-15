12f720f0bba072e23b04482b8a09582c9ecf1abb
Dillon Fagan <dillonfagan@pm.me>
2023-08-14 20:31:20 -0400

    Combine Chooser button and body when open

--- src/lib/components/Chooser.svelte
+++ src/lib/components/Chooser.svelte
@@ -28,9 +28,7 @@
 <div class="relative h-11" on:click|stopPropagation={() => {}} on:keypress={() => {}}>
 	<button
 		on:click={toggleDropdown}
-		class="{dropdownHidden
-			? 'bg-indigo-100 hover:bg-indigo-50'
-			: 'bg-primary'} px-3 py-1 h-full w-44 rounded brutal hovers font-bold font-display text-left outline-none"
+		class="bg-indigo-100 hover:bg-indigo-50 px-3 py-1 h-full w-44 rounded brutal hovers font-bold font-display text-left outline-none"
 	>
 		<span class="mr-1">
 			<svg
@@ -52,6 +50,7 @@
 	<ChooserBody
 		hidden={dropdownHidden}
 		title={$t('Category')}
+		chosenOption={isEnglish ? chosenOption : $t(chosenOption)}
 		options={isEnglish ? options : options.map(option => $t(option))}
 		onOptionClick={optionChosen}
 		onClose={toggleDropdown}
--- src/lib/components/Chooser/ChooserBody.svelte
+++ src/lib/components/Chooser/ChooserBody.svelte
@@ -3,6 +3,7 @@
 	import ChooserItem from './ChooserItem.svelte';
 
   export let title: string;
+  export let chosenOption: string;
   export let options: string[];
   export let hidden: boolean;
   export let onOptionClick: (option: string) => void;
@@ -11,7 +12,7 @@
 
 <div
   class:body-hidden={hidden}
-  class="fixed top-0 left-0 w-full h-full overflow-hidden md:h-fit md:absolute md:top-14 bg-indigo-50 md:brutal md:hovers-static md:rounded-md flex flex-col md:gap-y-2 z-50"
+  class="fixed top-0 left-0 w-full h-full overflow-hidden md:h-fit md:absolute bg-indigo-50 md:brutal md:hovers-above md:rounded-md flex flex-col md:gap-y-2 z-50"
 >
   <div class="md:hidden">
     <div class="p-4 bg-primary text-2xl font-bold text-left sticky top-0">
@@ -22,10 +23,31 @@
     </div>
     <hr class="border-black border-opacity-20" />
   </div>
+  <button
+		on:click={onClose}
+		class="hidden md:block bg-primary px-3 py-1 h-11 w-full border-b-2 border-black font-bold font-display text-left outline-none"
+	>
+		<span class="mr-1">
+			<svg
+				class="inline rotate-180"
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="3"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			  ><polyline points="6 9 12 15 18 9" /></svg
+			>
+		</span>
+		<span>{chosenOption}</span>
+	</button>
   <div class="flex flex-col overflow-y-scroll">
     {#each options as option}
-      <ChooserItem on:click={() => onOptionClick(option)}>{option}</ChooserItem>
       <hr />
+      <ChooserItem on:click={() => onOptionClick(option)}>{option}</ChooserItem>
     {/each}
   </div>
 </div>
