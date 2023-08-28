b13f0bfa2a3da2b4e3e98806a7e5a89b153eebe5
Jeannie McGill <jeanniecaitlyn@gmail.com>
2023-08-28 19:30:52 -0400

    Update spacing

--- src/lib/views/SearchInputView.svelte
+++ src/lib/views/SearchInputView.svelte
@@ -3,7 +3,7 @@
 	import { t } from '$lib/language/translate';
 	import { searchFilter } from '$lib/stores/catalog';
 	import XMarkIcon from '$lib/icons/x-mark.svg';
-    import SearchIcon from '$lib/icons/magnifying-glass.svg';
+	import SearchIcon from '$lib/icons/magnifying-glass.svg';
 
 	const clearSearch = () => {
 		$searchFilter = '';
@@ -11,11 +11,11 @@
 </script>
 
 <div class="relative">
-    <img src={SearchIcon} alt="searchIcon" class="w-6 h-6 absolute top-2.5 left-2 transform">
-    <TextInput bind:value={$searchFilter} placeholder={$t('Input.Search')} />
-    {#if $searchFilter.length > 0}
-	    <button class="btn btn-circle btn-sm btn-ghost absolute right-2 top-1.5" on:click={clearSearch}>
-		    <img src={XMarkIcon} alt="searchIcon" class="w-5 h-5 absolute top-1.75 left-2.25 transform">
-	    </button>
-    {/if}
+	<img src={SearchIcon} alt="searchIcon" class="w-6 h-6 absolute top-2.5 left-2 transform" />
+	<TextInput bind:value={$searchFilter} placeholder={$t('Input.Search')} />
+	{#if $searchFilter.length > 0}
+		<button class="btn btn-circle btn-sm btn-ghost absolute right-2 top-1.5" on:click={clearSearch}>
+			<img src={XMarkIcon} alt="searchIcon" class="w-5 h-5 absolute top-1.75 left-2.25 transform" />
+		</button>
+	{/if}
 </div>
