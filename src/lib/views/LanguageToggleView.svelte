98296c8c180182bbe0bd95539aa44d523edcea36
Dillon Fagan <dillonfagan@pm.me>
2023-09-15 20:18:58 -0400

    Show opposite locale for toggle

--- src/lib/views/LanguageToggleView.svelte
+++ src/lib/views/LanguageToggleView.svelte
@@ -1,11 +1,13 @@
 <script>
 	import { locale } from '$lib/language/translate';
+
+	$: otherLocale = $locale === 'en' ? 'es' : 'en';
 </script>
 
 <button
-	on:click={() => ($locale = $locale === 'en' ? 'es' : 'en')}
+	on:click={() => $locale = otherLocale}
 	class="btn btn-ghost btn-circle font-sans text-lg"
 	aria-label="Language"
 >
-	{$locale.toUpperCase()}
+	{otherLocale.toUpperCase()}
 </button>
