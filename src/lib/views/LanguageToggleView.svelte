4283a305778a2f5d00b286f2c185114d01ff496d
Dillon Fagan <dillonfagan@pm.me>
2023-09-15 20:08:17 -0400

    Use text instead of flag emoji for language

--- src/lib/views/LanguageToggleView.svelte
+++ src/lib/views/LanguageToggleView.svelte
@@ -1,14 +1,11 @@
 <script>
 	import { locale } from '$lib/language/translate';
-
-	const britishFlag = '🏴󠁧󠁢󠁥󠁮󠁧󠁿';
-	const spanishFlag = '🇪🇸';
 </script>
 
 <button
 	on:click={() => ($locale = $locale === 'en' ? 'es' : 'en')}
-	class="btn btn-ghost btn-circle text-3xl"
+	class="btn btn-ghost btn-circle font-sans text-lg"
 	aria-label="Language"
 >
-	{$locale === 'en' ? spanishFlag : britishFlag}
+	{$locale.toUpperCase()}
 </button>
